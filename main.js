f11acd33040408f240ad97247d1bc1ab180170e9
Harindu Nimshan <it21004186@my.sliit.lk>
2023-08-09 00:05:19 +0530

    Developed the functionality in Contact form using EmailJS Dependency. And Update the file structure.

--- main.js
+++ main.js
@@ -21,3 +21,51 @@ var tablinks = document.getElementsByClassName("tab-links");
         function closemenu(){
             sidemenu.style.right = "-200px";
         }
+
+//Get all needed elements from the DOM
+const contactForm = document.querySelector("#contact-form");
+const submitBtn = document.querySelector(".submit-btn");
+const nameInput = document.querySelector("#user_name");
+const emailInput = document.querySelector("#user_email");
+const subjectInput = document.querySelector("#subject");
+const messageInput = document.querySelector("#message");
+
+//Get needed data from email JS
+const publicKey = "51eAbHxO25TsRhYFr";
+const serviceID = "service_fnztb84";
+const templateID = "template_p8kzqfh";
+
+//Initialize email JS with public key
+emailjs.init(publicKey);
+
+//Add submit event to the form
+contactForm.addEventListener("submit", e => {
+    //Prevent from default behaviour
+    e.preventDefault();
+    //Change button text
+    submitBtn.innerText = "Just A Moment...";
+    //Get all input field values
+    const inputFields = {
+        name: nameInput.value,
+        email: emailInput.value,
+        subject: subjectInput.value,
+        message: messageInput.value
+    }
+    // Send the email
+    // (Add service, template ID and input Field values)
+    emailjs.send(serviceID, templateID, inputFields)
+        .then(() => {
+            //Change Button Text
+            submitBtn.innerText = "Done";
+            //Clear out all input fields
+            nameInput.value = "";
+            emailInput.value = "";
+            subjectInput.value = "";
+            messageInput.value = "";
+        }, (error) => {
+            //console log the error
+            console.log(error);
+            //Change Button Text
+            submitBtn.innerText = "Faild!";
+        });
+});
